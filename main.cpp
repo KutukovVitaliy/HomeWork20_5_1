b3284a54fdc51af9a8954384beea3dee0c5b8f59
kvitadmin <kutukov@list.ru>
2021-08-13 11:43:16 +0300

    Second commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <fstream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string FirstName, LastName, DateOfPayment;
+    bool ValueTrue;
+    int Payment;
+    std::cout << "Enter the first name: ";
+    std::cin >> FirstName;
+    std::cout << "Enter the last name: ";
+    std::cin >> LastName;
+    do{
+        ValueTrue = true;
+        std::cout << "Enter the date of payment (dd-mm-yy): ";
+        std::cin >> DateOfPayment;
+        int dd = std::stoi(DateOfPayment.substr(0,2));
+        int mm = std::stoi(DateOfPayment.substr(3,2));
+        int yy = std::stoi(DateOfPayment.substr(6,2));
+        if(dd < 1 || dd > 31 || mm < 1 || mm > 12 || yy < 0 || yy > 21){
+            std::cout << " Bad input. Try again!" << std::endl;
+            ValueTrue = false;
+        }
+    } while (!ValueTrue);
+    do{
+        ValueTrue = true;
+        std::cout << "Enter the value of payment: ";
+        std::cin >> Payment;
+        if(Payment <= 0){
+            std::cout << " Bad input. Try again!" << std::endl;
+            ValueTrue = false;
+        }
+    }while (!ValueTrue);
+    std::ofstream PaymentFile("../PaymentFile.txt",std::ios::app);
+    PaymentFile << FirstName << " " << LastName << " " << DateOfPayment << " " << Payment << std::endl;
+
     return 0;
 }
