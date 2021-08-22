417f94324092f6f704f9401155fcfbec02e5ecd1
kvit <kutukov@list.ru>
2021-08-22 16:43:10 +0300

    fixed variableы names

--- main.cpp
+++ main.cpp
@@ -2,36 +2,39 @@
 #include <fstream>
 
 int main() {
-    std::string FirstName, LastName, DateOfPayment;
-    bool ValueTrue;
-    int Payment;
+    std::string firstName, lastName, dateOfPayment;
+    bool valueTrue;
+    int payment;
     std::cout << "Enter the first name: ";
-    std::cin >> FirstName;
+    std::cin >> firstName;
     std::cout << "Enter the last name: ";
-    std::cin >> LastName;
+    std::cin >> lastName;
     do{
-        ValueTrue = true;
+        valueTrue = true;
         std::cout << "Enter the date of payment (dd-mm-yy): ";
-        std::cin >> DateOfPayment;
-        int dd = std::stoi(DateOfPayment.substr(0,2));
-        int mm = std::stoi(DateOfPayment.substr(3,2));
-        int yy = std::stoi(DateOfPayment.substr(6,2));
+        std::cin >> dateOfPayment;
+        int dd = std::stoi(dateOfPayment.substr(0, 2));
+        int mm = std::stoi(dateOfPayment.substr(3, 2));
+        int yy = std::stoi(dateOfPayment.substr(6, 2));
         if(dd < 1 || dd > 31 || mm < 1 || mm > 12 || yy < 0 || yy > 21){
             std::cout << " Bad input. Try again!" << std::endl;
-            ValueTrue = false;
+            valueTrue = false;
         }
-    } while (!ValueTrue);
+    } while (!valueTrue);
     do{
-        ValueTrue = true;
+        valueTrue = true;
         std::cout << "Enter the value of payment: ";
-        std::cin >> Payment;
-        if(Payment <= 0){
+        std::cin >> payment;
+        if(payment <= 0){
             std::cout << " Bad input. Try again!" << std::endl;
-            ValueTrue = false;
+            valueTrue = false;
         }
-    }while (!ValueTrue);
-    std::ofstream PaymentFile("../PaymentFile.txt",std::ios::app);
-    PaymentFile << FirstName << " " << LastName << " " << DateOfPayment << " " << Payment << std::endl;
-    PaymentFile.close();
+    }while (!valueTrue);
+    std::ofstream paymentFile("../PaymentFile.txt", std::ios::app);
+    if(paymentFile.is_open()){
+        paymentFile << firstName << " " << lastName << " " << dateOfPayment << " " << payment << std::endl;
+        paymentFile.close();
+    }
+    else std::cout << "Can't open file PaymentFile.txt" << std::endl;
     return 0;
 }
